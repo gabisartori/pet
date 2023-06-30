f2c13759361e5bfcd47f1d945f5458eecece8c74
Gabriel Sartori Rangel <73446902+gabisartori@users.noreply.github.com>
2023-06-30 17:24:59 -0300

    Exercise 2

---
+++ ex02.py
@@ -0,0 +1,10 @@
+# This piece of code asks for the user to input 2 numbers via command line
+# It'll sum them up and print the result
+# You may run it a couple times to identify what is going wrong with the output
+# And then change how to process the user's input so that the result will be correct
+
+a = input("Insert the first number: ")
+b = input("Inser the second number: ")
+sum = a + b
+
+print(f"The sum of {a} and {b} is {sum}")
